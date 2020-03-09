bec115224f7021996a6670883debbd204c76cb0e
Chechailiuk Volodymyr <chechailiuk@fabrica.net.ua>
2020-03-09 17:20:50 +0200

    docker exercises finished

--- docker/00_how_to_docker/docker_start.sh
+++ docker/00_how_to_docker/docker_start.sh
@@ -163,7 +163,7 @@ docker service scale marines=20
 # 31. Force quit and delete all the services on the local swarm, in one command.
 docker service rm $(docker service ls -q)
 # 32. Force quit and delete all the containers (whatever their status), in one command.
-docker container rm $(docker container ls -aq)
+docker container rm -f $(docker container ls -aq)
 # 33. Delete all the container images stored on the Char virtual machine, in one command
 # as well.
 docker rmi $(docker images -a -q)
